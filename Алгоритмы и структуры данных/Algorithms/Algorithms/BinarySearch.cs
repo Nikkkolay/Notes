e6bf49d8ab3cdbfa45efc791c35790e473cbed41
Nikolay Baturin <nikolay.baturin.998@mail.ru>
2022-01-31 22:25:08 +0300

    add: notes about binary search & update gitignore

---
+++ Алгоритмы и структуры данных/Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class BinarySearch
+    {
+        public static int FindIndexOfNumber(int desiredNumber, int[] arr)
+        {
+            var firstIndex = 0;
+            var lastIndex = arr.Length - 1;
+
+            while (firstIndex <= lastIndex)
+            {
+                var midIndex = (lastIndex + firstIndex) / 2;
+                var guess = arr[midIndex];
+                
+                if (guess == desiredNumber) return midIndex;
+                
+                if (guess < desiredNumber)
+                    firstIndex = midIndex + 1;
+                else
+                    lastIndex = midIndex - 1;
+            }
+
+            return -1;
+        }
+
+        public static void Test()
+        {
+            int[] arr = {1, 2, 5, 12, 89, 6};
+            Console.WriteLine(FindIndexOfNumber(5, arr));
+            Console.WriteLine(FindIndexOfNumber(6, arr));
+        }
+    }
+}
